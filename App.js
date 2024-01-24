31c6ed13fd282b1b262612fea88aec40981cf3f4
Lucrada <iamdracula.dracula@gmail.com>
2024-01-24 15:55:20 +0530

    added one more horizontal scroll bar and quick action section

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
+import { StyleSheet, ScrollView, View, Text } from 'react-native';
 import Header from './components/header/Header';
 import HorizontalScroller from './components/horizontal_scroller/HorizontalScroller';
 import VerticalSpacer from './components/spacer/VerticalSpacer';
@@ -6,13 +6,18 @@ import Actions from './components/actions/Actions';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.container}>
       <Header name="John Doe" />
       <VerticalSpacer amount={20} />
       <HorizontalScroller />
       <VerticalSpacer amount={20} />
       <Actions />
-    </View>
+      <VerticalSpacer amount={20} />
+      <HorizontalScroller />
+      <VerticalSpacer amount={20} />
+      <Actions />
+      <VerticalSpacer amount={20} />
+    </ScrollView>
   );
 }
 
