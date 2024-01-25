df44d5b3a69ca5b12bb725368c1440158333fb7b
Lucrada <iamdracula.dracula@gmail.com>
2024-01-25 15:59:37 +0530

    changed background colors and adjusted margins a little bit

--- components/ball controller/BallController.js
+++ components/ball controller/BallController.js
@@ -7,7 +7,7 @@ export default function BallController() {
 
     function moveUp() {
         Animated.timing(y_cord, {
-            toValue: 280,
+            toValue: 260,
             duration: 1000,
             useNativeDriver: false,
         }).start();
@@ -52,11 +52,11 @@ export default function BallController() {
 
     return (
         <View>
-            <View style={{ flex: 1, height: 700, justifyContent: "center", alignItems: "center" }}>
+            <View style={{ flex: 1, height: 700, justifyContent: "center", alignItems: "center", backgroundColor: '#333', borderRadius: 10, }}>
                 <Animated.View style={{ backgroundColor: 'red', borderRadius: 50, width: 100, height: 100, marginBottom: y_cord, marginLeft: x_cord }}>
                 </Animated.View>
             </View>
-            <View style={{ flexDirection: 'row', justifyContent: 'center', alignContent: 'center', marginBottom: 30, justifyContent: 'space-around' }}>
+            <View style={{ flexDirection: 'row', justifyContent: 'center', alignContent: 'center', marginBottom: 30, marginTop: 10, justifyContent: 'space-around' }}>
                 <Button onPress={moveUp} title="Move up" />
                 <Button onPress={moveDown} title="Move down" />
             </View>
